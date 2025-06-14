55c2eb16ae29935cc392703611c448eb8d5b4745
Ryan Jiro <92157016+ryanjiroo@users.noreply.github.com>
2025-06-14 20:13:28 +0700

    Update portfolio.ts

--- portfolio.ts
+++ portfolio.ts
@@ -178,6 +178,11 @@ export const experience: ExperienceType[] = [
 ];
 
 export const projects: ProjectType[] = [
+   {
+    name: "PelukDiri",
+    desc: "A web-based application serving as a mental health support platform and an empathetic virtual companion. This was a group project, and I took on the role of Front-End Developer. The application was built using ReactJS with Vite and TailwindCSS.",
+    link: "https://pelukdiri.vercel.app/",
+  },
   {
     name: "Fallout Activation",
     desc: "Web based application for Fallout Activation to manage Indihome and Indibiz problem report. Build with Laravel, NodeJS, Bootstrap, and MySQL.",
